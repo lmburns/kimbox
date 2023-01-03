a9464870f3927f5add56e7292f2ed25c0b3b9fcb
Lucas Burns <burnsac@me.com>
2023-01-03 16:20:24 -0600

    update: hex color values are now all uppercase; added jade_green as a color

--- lua/kimbox/palette.lua
+++ lua/kimbox/palette.lua
@@ -1,8 +1,8 @@
 local bgs = {
     medium = "#231A0C",
-    ocean = "#221a02",
-    vscode = "#221a0f",
-    deep = "#0f111b",
+    ocean = "#221A02",
+    vscode = "#221A0F",
+    deep = "#0F111B",
     darker = "#291804"
 }
 
@@ -11,13 +11,13 @@ return {
         bg1 = "#39260E",
         bg2 = "#362712",
         bg3 = "#291804",
-        bg4 = "#5e452b",
-        bg5 = "#5e452b",
+        bg4 = "#5E452B",
+        bg5 = "#5E452B",
         fg0 = "#D9AE80",
         fg1 = "#7E602C",
-        fg2 = "#5e452b",
+        fg2 = "#5E452B",
         fg3 = "#C2A383",
-        fg4 = "#e8c097",
+        fg4 = "#E8C097",
         ------------------------------------------------------------------------------------------------------
         black = "#000000",
         red = "#EF1D55",
@@ -28,39 +28,40 @@ return {
         aqua = "#7EB2B1",
         blue = "#4C96A8",
         purple = "#98676A",
+        --
         philippine_green = "#088649",
-        sea_green = "#77a172",
-        -- dark_seafoam = "#24a36a", -- "#36997a" "#3fba94" "#2ab074"
-        morning_blue = "#83a598",
+        sea_green = "#77A172",
+        jade_green = "#2AB074",
+        morning_blue = "#83A598",
         jelly_bean_blue = "#418292",
         slate_gray = "#719190",
         tuscan_red = "#7E5053",
-        maximum_purple = "#733e8b",
-        deep_lilac = "#a25bc4",
+        maximum_purple = "#733E8B",
+        deep_lilac = "#A25BC4",
         vista_blue = "#7E9CD8",
         amethyst = "#938AA9",
-        salmon = "#ea6962",
+        oni_violet = "#957FB8",
+        salmon = "#EA6962",
         wave_red = "#E46876",
         peach_red = "#FF5D62",
-        oni_violet = "#957FB8",
-        old_rose = "#bd798b",
-        puce = "#d3869b",
-        pumpkin = "#fe8019",
+        old_rose = "#BD798B",
+        puce = "#D3869B",
+        pumpkin = "#FE8019",
         surimi_orange = "#FFA066",
-        dyellow = "#e78a4e", -- "#db8456"
+        dyellow = "#E78A4E", -- "#DB8456"
         bg_red = "#DC3958",
-        light_red = "#cc6666",
+        light_red = "#CC6666",
         -- These are duplicates because they are both used and could be changed at some point
         coyote_brown = "#7E602C",
         coyote_brown1 = "#7E602C",
-        grullo_grey = "#a89984",
+        grullo_grey = "#A89984",
         wenge_grey = "#625A5A",
-        operator_base = "#e8c097",
+        operator_base = "#E8C097",
         ------------------------------------------------------------------------------------------------------
         diff_add = "#889B4A",
-        diff_delete = "#cc6666",
+        diff_delete = "#CC6666",
         diff_change = "#543739",
-        diff_text = "#325c59" -- "#335250" -- "#405453"
+        diff_text = "#325C59" -- "#335250" "#405453"
     },
     bgs = bgs
 }
